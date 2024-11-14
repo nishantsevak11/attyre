401dc8124492346e3b6639bfb419e659d1f31023
nishantsevak11 <160159405+nishantsevak11@users.noreply.github.com>
2024-11-14 20:40:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Pallate from './components/Pallate'; 
 import Footer from './components/Footer';
-import Home from './pages/home';
+import Home from './pages/Home';
 import PallatePage from './components/PallatePage';
 
 
